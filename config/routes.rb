cf06c371965c7bea334ed3c49572be12eda66784
ken100 <zhaoyang_100@hotmail.com>
2014-06-15 21:57:22 +0900

    not incloud chapter 12.2,12.3,13.*

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
+
+
   resources :orders
 
 
@@ -10,8 +21,9 @@ Depot::Application.routes.draw do
 
   get "store/index"
 
-  resources :products
-
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
